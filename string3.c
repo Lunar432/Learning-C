ed16bf25baad09b88f34422a0c0a0838553b5f6e
Adiba Muttaqin <139992483+Lunar432@users.noreply.github.com>
2023-08-13 11:19:09 +0600

    Update string3.c

--- string3.c
+++ string3.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <string.h>
+
+//use of strcat
+
+
 int main() {
    char str1[100] = "Hello ", str2[] = "World";
    strcat(str1, str2);
-
    puts(str1);
    puts(str2);
 
